2fc24020a1b1f759abb0ede6e7060435a813fac9
Nutthanich Narphromar <monaprom134@gmail.com>
2020-04-15 13:53:30 +0700

    Request 8 bullets from pool at once

--- src/BulletPool.java
+++ src/BulletPool.java
@@ -4,50 +4,46 @@ import java.util.List;
 public class BulletPool {
 
     private static BulletPool instance = new BulletPool();
-    private List<Bullet> bullets = new ArrayList<Bullet>();
+    //    private List<Bullet> bullets = new ArrayList<Bullet>();
+    private List<List<Bullet>> bullets = new ArrayList<List<Bullet>>();
 
-    private BulletPool() {}
+    private BulletPool() {
+    }
 
     public static BulletPool getInstance() {
         return instance;
     }
 
-//    public List<Bullet> acquireBullet() {
-//        System.out.println("---------------------------");
-//        System.out.println("Acquire " + bullets);
-//        System.out.println("---------------------------");
-////        System.out.println(setOfBullets);
-//        List<Bullet> returnBullets = new ArrayList<>();
-//        if(bullets.size() > 0) {
-//            System.out.println("Here");
-//            for(int i = 0; i < 8; i++) {
-//                returnBullets.add(bullets.get(i));
-//            }
-//            bullets.subList(0, 8).clear();
-//        }
-//        else {
-//            System.out.println("bullet size: " + bullets.size());
-//        }
-//        return returnBullets;
-////        return null;
-//    }
-
-    public Bullet acquireBullet() {
+    public List<Bullet> acquireBullet() {
         System.out.println("---------------------------");
         System.out.println("Acquire " + bullets);
         System.out.println("---------------------------");
+//        System.out.println(setOfBullets);
+//        List<Bullet> returnBullets = new ArrayList<Bullet>();
         if (bullets.size() > 0) {
-            Bullet bullet = bullets.get(0);
-            bullets.remove(0);
-            return bullet;
+            List<Bullet> first = bullets.get(0);
+            bullets.remove(first);
+            return first;
         }
         return null;
     }
 
-    public void releaseBullet(Bullet bullet) {
+//    public Bullet acquireBullet() {
+//        System.out.println("---------------------------");
+//        System.out.println("Acquire " + bullets);
+//        System.out.println("---------------------------");
+//        if (bullets.size() > 0) {
+//            Bullet bullet = bullets.get(0);
+//            bullets.remove(0);
+//            return bullet;
+//        }
+//        return null;
+//    }
+
+    public void releaseBullet(List<Bullet> bulletList) {
         System.out.println("---------------------------");
-        bullets.add(bullet);
-        System.out.println("Release: " + bullets);
+        bullets.add(bulletList);
+        System.out.println("Release: " + bulletList);
         System.out.println("---------------------------");
     }
 }
